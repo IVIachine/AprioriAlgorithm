2e9734c4eb2d1b342850bbb3a0fa089bf41918ce
Adam Gurman <gurman8r@gmail.com>
2015-12-08 20:32:41 -0500

    formatting

--- Apriori/functions.cpp
+++ Apriori/functions.cpp
@@ -5,11 +5,14 @@ void mainMenu()
 	string fileName = "";
 	cout << "Please enter the file you would like to load in: ";
 	cin >> fileName;
+
 	ifstream fin(fileName);
+
 	do
 	{
-	cout << "Please enter valid file name: ";
-	cin >> fileName;
-	fin.open(fileName);
-	} while (fin.fail());
+		cout << "Please enter valid file name: ";
+		cin >> fileName;
+		fin.open(fileName);
+	} 
+	while (fin.fail());
 }
